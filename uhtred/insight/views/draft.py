626b55575420832b72772608e861f44923a2fae8
Uhtred M <am@uhtred.dev>
2023-08-25 23:43:25 +0100

    insight draft preview

--- uhtred/insight/views/draft.py
+++ uhtred/insight/views/draft.py
@@ -8,6 +8,9 @@ from uhtred.insight.serializers import InsightDetail
 
 @api_view(['GET'])
 def draft_preview(request: Request, insight_id: int) -> Response:
-    insight: Insight = get_object_or_404(Insight, id=insight_id)
+    insight: Insight = get_object_or_404(
+        Insight,
+        published_at__isnull=True,
+        id=insight_id)
     serializer = InsightDetail(insight)
     return Response(serializer.data)
